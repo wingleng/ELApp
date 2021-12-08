05515a696a1ff9a36dc7ab1af3e2d7cd92021d6f
wongGNOW <923084107@qq.com>
2021-12-08 16:55:57 +0800

    feat(application):完成了使用相机拍照的功能

--- app/src/main/java/com/wong/elapp/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/wong/elapp/ui/notifications/NotificationsFragment.java
@@ -1,9 +1,15 @@
 package com.wong.elapp.ui.notifications;
 
+import static android.app.Activity.RESULT_OK;
+
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +21,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -30,6 +37,8 @@ import com.wong.elapp.databinding.FragmentNotificationsBinding;
 import com.wong.elapp.ui.home.HomeViewModel;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 
 public class NotificationsFragment extends Fragment {
@@ -37,15 +46,16 @@ public class NotificationsFragment extends Fragment {
     private HomeViewModel homeViewModel;
     private FragmentNotificationsBinding binding;
     private Context context;
-    private TextView textView;
 
     private Button cameBtn;
-
     private Button blumBtn;
-
     private ImageView imageView;
 
     private final int BLUM_SELECT=100;
+    private final int CAMER_CAPTURE=101;
+    private Uri imageUri;
+    private final String filePath = Environment.getExternalStorageDirectory() + File.separator + "output_image.jpg";
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -54,7 +64,7 @@ public class NotificationsFragment extends Fragment {
         View root = binding.getRoot();
         context = root.getContext();
 
-        textView = binding.textNotifications;
+
         cameBtn = binding.takepic;
         blumBtn = binding.blum;
         imageView = binding.imageView;
@@ -77,30 +87,68 @@ public class NotificationsFragment extends Fragment {
         if (requestCode == BLUM_SELECT){
             Glide.with(context).load(data.getData()).into(imageView);
         }
+        if (requestCode == CAMER_CAPTURE && resultCode == RESULT_OK){
+            try {
+                Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(imageUri));
+                imageView.setImageBitmap(bitmap);
+                //将图片解析成Bitmap对象，并把它显现出来
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void getPermission(){
+        PermissionX.init(NotificationsFragment.this)
+                .permissions(Manifest.permission.CAMERA,Manifest.permission.ACCESS_NETWORK_STATE,Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .onExplainRequestReason(new ExplainReasonCallbackWithBeforeParam() {
+                    @Override
+                    public void onExplainReason(@NonNull ExplainScope scope, @NonNull List<String> deniedList, boolean beforeRequest) {
+                        scope.showRequestReasonDialog(deniedList,"相机权限是必须的","明白","取消");
+                    }
+                })
+                .request(new RequestCallback() {
+                    @Override
+                    public void onResult(boolean allGranted, @NonNull List<String> grantedList, @NonNull List<String> deniedList) {
+                        if(allGranted){
+                            Toast.makeText(context,"所有权限通过",Toast.LENGTH_LONG).show();
+                        }else{
+                            Toast.makeText(context,"您拒绝了如下权限："+deniedList,Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 
     class CamerBtnListener implements View.OnClickListener{
         @Override
         public void onClick(View v) {
             //首先申请相机权限：
-            PermissionX.init(NotificationsFragment.this)
-                    .permissions(Manifest.permission.CAMERA)
-                    .onExplainRequestReason(new ExplainReasonCallbackWithBeforeParam() {
-                        @Override
-                        public void onExplainReason(@NonNull ExplainScope scope, @NonNull List<String> deniedList, boolean beforeRequest) {
-                            scope.showRequestReasonDialog(deniedList,"相机权限是必须的","明白","取消");
-                        }
-                    })
-                    .request(new RequestCallback() {
-                        @Override
-                        public void onResult(boolean allGranted, @NonNull List<String> grantedList, @NonNull List<String> deniedList) {
-                            if(allGranted){
-                                Toast.makeText(context,"所有权限通过",Toast.LENGTH_LONG).show();
-                            }else{
-                                Toast.makeText(context,"您拒绝了如下权限："+deniedList,Toast.LENGTH_LONG).show();
-                            }
-                        }
-                    });
+            getPermission();
+
+            File outputImage = new File(filePath);
+
+            //判断图片文件是否存在
+            try {
+                if(!outputImage.getParentFile().exists()){
+                    outputImage.getParentFile().mkdir();
+                }
+                if(outputImage.exists()){
+                    outputImage.delete();
+                }
+                outputImage.createNewFile();
+
+                //获取Uri(必须是在API24才能使用这种方法，其他情况下直接获取就好了。
+                imageUri = FileProvider.getUriForFile(context,"com.wong.elapp.fileprovider",outputImage);
+
+                //Intent打开相机
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+
+                startActivityForResult(intent,CAMER_CAPTURE);
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
         }
     }
 
