61928630e4ffba7dbcc7169584535769a45c4643
wongGNOW <923084107@qq.com>
2021-12-12 01:24:58 +0800

    fix:将拍照功能移动到dash2界面

--- app/src/main/java/com/wong/elapp/ui/dashboard/childrens/dash_secondFragment.java
+++ app/src/main/java/com/wong/elapp/ui/dashboard/childrens/dash_secondFragment.java
@@ -1,12 +1,44 @@
 package com.wong.elapp.ui.dashboard.childrens;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.permissionx.guolindev.PermissionX;
+import com.permissionx.guolindev.callback.ExplainReasonCallbackWithBeforeParam;
+import com.permissionx.guolindev.callback.RequestCallback;
+import com.permissionx.guolindev.request.ExplainScope;
+import com.qmuiteam.qmui.widget.roundwidget.QMUIRoundButton;
 import com.wong.elapp.R;
+import com.wong.elapp.databinding.FragmentDashSecondBinding;
+import com.wong.elapp.ui.home.HomeViewModel;
+import com.wong.elapp.ui.notifications.NotificationsFragment;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +55,22 @@ public class dash_secondFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    //TODO:用户定义的变量
+    HomeViewModel homeViewModel;
+    FragmentDashSecondBinding binding;
+
+    QMUIRoundButton dash2Camer;
+    QMUIRoundButton dash2Blum;
+    ImageView dash2Img;
+    Context context;
+
+
+    private final int BLUM_SELECT=100;
+    private final int CAMER_CAPTURE=101;
+    private Uri imageUri;
+    private final String filePath = Environment.getExternalStorageDirectory() + File.separator + "output_image.jpg";//文件默认缓存路径
+
 
     public dash_secondFragment() {
         // Required empty public constructor
@@ -59,6 +107,115 @@ public class dash_secondFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dash_second, container, false);
+        homeViewModel = new ViewModelProvider(requireActivity()).get(HomeViewModel.class);
+        binding = FragmentDashSecondBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        context = root.getContext();
+        //初始化组件
+        dash2Camer = binding.dash2Camer;
+        dash2Blum = binding.dash2Blum;
+        dash2Img = binding.dash2Img;
+
+        dash2Camer.setOnClickListener(new dash_secondFragment.CamerBtnListener());
+        dash2Blum.setOnClickListener(new dash_secondFragment.BlumBtnListeber());
+
+
+        return root;
     }
+
+    /**
+     * 设置回调
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == BLUM_SELECT){
+            Glide.with(context).load(data.getData()).into(dash2Img);
+        }
+
+
+        if (requestCode == CAMER_CAPTURE && resultCode == RESULT_OK){
+            try {
+                Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(imageUri));
+                dash2Img.setImageBitmap(bitmap);
+                //将图片解析成Bitmap对象，并把它显现出来
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 权限申请
+     */
+    private void getPermission(){
+        PermissionX.init(dash_secondFragment.this)
+                .permissions(Manifest.permission.CAMERA,Manifest.permission.ACCESS_NETWORK_STATE,Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .onExplainRequestReason(new ExplainReasonCallbackWithBeforeParam() {
+                    @Override
+                    public void onExplainReason(@NonNull ExplainScope scope, @NonNull List<String> deniedList, boolean beforeRequest) {
+                        scope.showRequestReasonDialog(deniedList,"相机权限是必须的","明白","取消");
+                    }
+                })
+                .request(new RequestCallback() {
+                    @Override
+                    public void onResult(boolean allGranted, @NonNull List<String> grantedList, @NonNull List<String> deniedList) {
+                        if(allGranted){
+                            Toast.makeText(context,"所有权限通过",Toast.LENGTH_LONG).show();
+                        }else{
+                            Toast.makeText(context,"您拒绝了如下权限："+deniedList,Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    /**
+     * 使用相机进行拍摄
+     */
+    class CamerBtnListener implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            //首先申请相机权限：
+            getPermission();
+
+            File outputImage = new File(filePath);
+
+            //判断图片文件是否存在
+            try {
+                if(!outputImage.getParentFile().exists()){
+                    outputImage.getParentFile().mkdir();
+                }
+                if(outputImage.exists()){
+                    outputImage.delete();
+                }
+                outputImage.createNewFile();
+
+                //获取Uri(必须是在API24才能使用这种方法，其他情况下直接获取就好了。
+                imageUri = FileProvider.getUriForFile(context,"com.wong.elapp.fileprovider",outputImage);
+
+                //Intent打开相机
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+
+                startActivityForResult(intent,CAMER_CAPTURE);
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+
+    /**
+     * 从相册中选择图片
+     */
+    class BlumBtnListeber implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            Intent gallery = new Intent(Intent.ACTION_PICK);
+            gallery.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+            startActivityForResult(gallery, BLUM_SELECT);
+        }
+    }
+
 }
