f09b0efb027d420f40bdff1de41ea2ff9cf7c20f
wongGNOW <923084107@qq.com>
2021-12-11 11:37:49 +0800

    feat(DashFragment):第二个界面大改完成

--- app/src/main/java/com/wong/elapp/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/wong/elapp/ui/dashboard/DashboardFragment.java
@@ -2,9 +2,12 @@ package com.wong.elapp.ui.dashboard;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,9 +17,18 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+import com.qmuiteam.qmui.widget.tab.QMUITabSegment;
 import com.wong.elapp.R;
 import com.wong.elapp.databinding.FragmentDashboardBinding;
 import com.wong.elapp.hilt.types.LocalMapper;
@@ -27,6 +39,8 @@ import com.wong.elapp.pojo.RandomList;
 import com.wong.elapp.pojo.vo.LoginParam;
 import com.wong.elapp.pojo.vo.Result;
 import com.wong.elapp.pojo.vo.Youdaoresult.YoudaoResult;
+import com.wong.elapp.ui.dashboard.childrens.dash_firstFragment;
+import com.wong.elapp.ui.dashboard.childrens.dash_secondFragment;
 import com.wong.elapp.ui.home.HomeViewModel;
 import com.wong.elapp.utils.ERRCODE;
 import com.wong.elapp.utils.WebError;
@@ -34,6 +48,7 @@ import com.wong.elapp.utils.WebError;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -57,6 +72,14 @@ public class DashboardFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private FragmentDashboardBinding binding;
+    private ViewPager2 contentViewPager;
+    private TabLayout tabLayout;
+
+
+    private List<Fragment> fragments = new ArrayList<>();
+    private List<String> titles = new ArrayList<>();
+    private final int TEXT_TRANSLATE = 0;
+    private final int PIC_TRANSLATE = 1;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -64,47 +87,19 @@ public class DashboardFragment extends Fragment {
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textView;
-        final Button tranlBtn = binding.textTranslate;
+        contentViewPager = binding.contentViewPager;
+        tabLayout = binding.tabLayout;
 
 
-        /**
-         * 有道翻译接口的测试
-         */
-        tranlBtn.setOnClickListener(v -> {
+        fragments.add(dash_firstFragment.newInstance("1","1"));
+        fragments.add(dash_secondFragment.newInstance("2","2"));
+        titles.add("文字翻译");
+        titles.add("图片翻译");
 
-            String APP_KEY = "5b9c6473842434f1";
-            String APP_SECRET = "DePFvNrafw6FKqFzYKYJsXrF9fOXmLXl";
+        //这里用了ViewPager2，和ViewPager的区别就是，只需要传递进去Activity或者Fragment，它会自动生成FragmentManager
+        contentViewPager.setAdapter(new MyFragmentAdapter(this));
 
-            Map<String,String> params = new HashMap<String,String>();
-            String q = "world";
-            String salt = String.valueOf(System.currentTimeMillis());
-            params.put("from", "en");
-            params.put("to", "zh-CHS");
-            params.put("signType", "v3");
-            String curtime = String.valueOf(System.currentTimeMillis() / 1000);
-            params.put("curtime", curtime);
-            String signStr = APP_KEY + truncate(q) + salt + curtime + APP_SECRET;
-            String sign = getDigest(signStr);
-            params.put("appKey", APP_KEY);
-            params.put("q", q);
-            params.put("salt", salt);
-            params.put("sign", sign);
-            Log.i("发送请求","发送请求");
-
-            Call<YoudaoResult> call = youdaoService.textTranslate(params);
-            call.enqueue(new Callback<YoudaoResult>() {
-                @Override
-                public void onResponse(Call<YoudaoResult> call, Response<YoudaoResult> response) {
-                    Log.i("返回的数据：",response.body()==null?"数据为空":response.body().getBasic().getExplains().get(0));
-                }
-
-                @Override
-                public void onFailure(Call<YoudaoResult> call, Throwable t) {
-                    Log.i(ERRCODE.REQUEST_FAILED.getMsgtype(), ERRCODE.REQUEST_FAILED.getMsg());
-                }
-            });
-        });
+        initTabLayou();
 
         return root;
     }
@@ -116,42 +111,83 @@ public class DashboardFragment extends Fragment {
     }
 
     /**
-     * 生成加密字段
+     * 配置TabLayout
      */
-    public static String getDigest(String string) {
-        if (string == null) {
-            return null;
+    public void initTabLayou(){
+        for (int i = 0; i < titles.size(); i++) {
+            tabLayout.addTab(tabLayout.newTab().setText(titles.get(i)), false);
         }
-        char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-        byte[] btInput = string.getBytes(StandardCharsets.UTF_8);
-        try {
-            MessageDigest mdInst = MessageDigest.getInstance("SHA-256");
-            mdInst.update(btInput);
-            byte[] md = mdInst.digest();
-            int j = md.length;
-            char str[] = new char[j * 2];
-            int k = 0;
-            for (byte byte0 : md) {
-                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
-                str[k++] = hexDigits[byte0 & 0xf];
+        tabLayout.setSelectedTabIndicatorColor(getResources().getColor(R.color.app_color_blue));
+        tabLayout.setTabIndicatorFullWidth(false);
+        tabLayout.addOnTabSelectedListener(new TabLayouLis());
+        new TabLayoutMediator(tabLayout, contentViewPager, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                tab.setText(titles.get(position));
             }
-            return new String(str);
-        } catch (NoSuchAlgorithmException e) {
-            return null;
+        }).attach();
+    }
+
+    /**
+     * 一个ViewPager2适配器，不过这个是适配Fragment的。
+     */
+    class MyFragmentAdapter extends FragmentStateAdapter{
+        public MyFragmentAdapter(@NonNull FragmentActivity fragmentActivity) {
+            super(fragmentActivity);
+        }
+
+        public MyFragmentAdapter(@NonNull Fragment fragment) {
+            super(fragment);
+        }
+
+        public MyFragmentAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+            super(fragmentManager, lifecycle);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            switch (position){
+                case TEXT_TRANSLATE:
+                    return fragments.get(TEXT_TRANSLATE);
+                case PIC_TRANSLATE:
+                    return fragments.get(PIC_TRANSLATE);
+                default:
+                    return fragments.get(TEXT_TRANSLATE);//默认这个。。总感觉这个会有问题。
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return fragments.size();
         }
     }
 
     /**
-     * 关键词操作，超过20个字符时需要操作。。
-     * @param q
-     * @return
+     * tabLayout的监听器
      */
-    public static String truncate(String q) {
-        if (q == null) {
-            return null;
+    class TabLayouLis implements TabLayout.OnTabSelectedListener{
+        @Override
+        public void onTabSelected(TabLayout.Tab tab) {
+            //为选中的选项加点特效。
+            TextView textView = new TextView(getActivity());
+            float selectedSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, 20, getResources().getDisplayMetrics());
+            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, selectedSize);
+            textView.setTextColor(getResources().getColor(R.color.app_color_blue_pressed));
+            textView.setText(tab.getText());
+            textView.setGravity(Gravity.CENTER_HORIZONTAL);
+            textView.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
+            tab.setCustomView(textView);
         }
-        int len = q.length();
-        String result;
-        return len <= 20 ? q : (q.substring(0, 10) + len + q.substring(len - 10, len));
+
+        @Override
+        public void onTabUnselected(TabLayout.Tab tab) {
+            tab.setCustomView(null);
+        }
+
+        @Override
+        public void onTabReselected(TabLayout.Tab tab) {
+
+        }
     }
 }
