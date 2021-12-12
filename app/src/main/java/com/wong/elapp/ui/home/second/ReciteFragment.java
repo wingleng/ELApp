cc3421f66cd19f020900aa24487fe8e1fc670987
wongGNOW <923084107@qq.com>
2021-12-13 01:04:29 +0800

    fix:修改了一下ViewPager

--- app/src/main/java/com/wong/elapp/ui/home/second/ReciteFragment.java
+++ app/src/main/java/com/wong/elapp/ui/home/second/ReciteFragment.java
@@ -13,9 +13,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 
 import com.qmuiteam.qmui.layout.QMUIButton;
+import com.qmuiteam.qmui.util.QMUIToastHelper;
 import com.qmuiteam.qmui.widget.roundwidget.QMUIRoundButton;
 import com.wong.elapp.R;
 import com.wong.elapp.databinding.FragmentReciteBinding;
@@ -24,6 +26,7 @@ import com.wong.elapp.ui.home.HomeViewModel;
 import com.wong.elapp.utils.DensityUtil;
 import com.wong.elapp.utils.cardtransformer.AlphaAndScalePageTransformer;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -37,12 +40,20 @@ public class ReciteFragment extends Fragment {
     @Inject
     DensityUtil densityUtil;
 
+
     FragmentReciteBinding binding;
     HomeViewModel homeViewModel;
     ViewPager2 viewPager2;
     QMUIRoundButton btnLeft;
     QMUIRoundButton btnRight;
     boolean isDragging;
+
+    //存储各自的List
+    private List<String> forget_list = new ArrayList<>();
+    private List<String> remember_list = new ArrayList<>();
+
+    private int WORDSIZE;//列表长度
+
 
     public ReciteFragment() {
         // Required empty public constructor
@@ -68,6 +79,7 @@ public class ReciteFragment extends Fragment {
 
         //对viewpager设置适配器：
         List<RandomList> rlist = homeViewModel.getList_word().getValue();//从viewmodel中获取到数据
+        WORDSIZE = rlist.size();//存一下数据长度
         viewPager2.setAdapter(new ViewPagerAdapter(rlist));//设置适配器,需要把数据和当前fragment或者activity的上下文传进去，因为要使用音乐播放器。
 
 
@@ -83,7 +95,29 @@ public class ReciteFragment extends Fragment {
 
 
         //配置页面滑动以及监听器
-//        viewPager2.setUserInputEnabled(false);//禁止用户进行滑动
+        viewPager2.setUserInputEnabled(false);//禁止用户进行滑动，为了方便两个按钮。。。
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                Log.i("当前选择的位置：","position"+position);
+                forget_list.add(rlist.get(position).getId());
+                if (position == WORDSIZE-1){//到达底部，发送请求，并且弹出提示框，返回主界面。。
+                    QMUIToastHelper.show(Toast.makeText(getActivity(),"到底了~"+forget_list.size(),Toast.LENGTH_LONG));
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                super.onPageScrollStateChanged(state);
+            }
+        });
 
         //设置按钮监听器,点击就模拟滑动
         btnLeft.setOnClickListener(v -> {
